76dbb553748e8937537b61d48dd525f3340c9710
Joshua De Chavez <75658658+Zalinto@users.noreply.github.com>
2024-05-22 11:14:45 -0700

    progress

---
+++ 01-Fundamentals-Part-1/starter/assignment.js
@@ -0,0 +1,15 @@
+const country = 'United States of America';
+const continent = 'North America';
+let population = '10000000';
+
+const isIsland = false;
+let language;
+
+// log type of variable
+console.log(typeof isIsland);
+console.log(typeof country);
+console.log(typeof continent);
+console.log(typeof population);
+console.log(typeof language);
+
+language = 'English';
---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,18 @@
+// function getBMI({mass, height}) {
+//     return mass / height ** 2;
+// }
+
+const getBMI = ({mass, height}) => {
+    return mass / (height ** 2);
+};
+
+let [markMass, markHeight] = [78, 1.69];
+let [johnMass, johnHeight] = [92, 1.95];
+
+const markHigherBMI = getBMI({mass: markMass, height: markHeight}) > getBMI({mass: johnMass, height: johnHeight});
+
+if (markHigherBMI) {
+    console.log(`Mark's BMI (${getBMI({mass: markMass, height: markHeight})}) is higher than John's!`);
+} else {
+    console.log(`John's BMI (${getBMI({mass: johnMass, height: johnHeight})}) is higher than Mark's!`);
+}
