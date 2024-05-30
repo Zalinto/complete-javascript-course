f5f6b67233b4b28343bdd878af7013e0275b885a
Joshua De Chavez <75658658+Zalinto@users.noreply.github.com>
2024-05-29 20:43:38 -0700

    progress

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -1,10 +1,34 @@
 'use strict';
 
+var firstName = 'Matilda';
 
-// console.log(a);
-// console.log(b);
-console.log(c);
+const josh = {
+    firstName: 'Joshua',
+    year: 2000, 
+    calcAge: function() {
+        console.log(2024 - this.year);
 
-const a = "a";
-let b = "b";
-var c = "c";
+        const isMillenial = () => {
+            console.log(this);
+            console.log(this.year >= 1981 && this.year <= 1996);
+        }
+        isMillenial(); // regular function call so this points to undefined
+    },
+
+    greet: () => {
+        console.log(`Hey ${this}`)
+        console.log(`Hey ${this.firstName}`)
+    },
+};
+
+josh.greet();
+josh.calcAge();
+
+const addExpr = function (a,b) {
+    console.log(arguments);
+    return a + b;
+}
+
+addExpr(2,5);
+
+var addArrow = (a,b) => a+b;
